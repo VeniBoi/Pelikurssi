b9540f12d12f3d2432db371b9fa558d109eadf1f
VeniBoi <35433655+VeniBoi@users.noreply.github.com>
2018-02-13 23:47:34 +0200

    Veni
    Sienien respawn

---
+++ UNITY/Pelikurssi/Assets/Scripts/SieniSpawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SieniSpawner : MonoBehaviour
+{
+	public GameObject[] spawnees;
+	public Transform spawnPos;
+
+
+	int randomInt;
+
+	// Use this for initialization
+	void Start()
+	{
+		randomInt = Random.Range(0, spawnees.Length);
+		Instantiate(spawnees[randomInt], spawnPos.position, Quaternion.Euler(270, 0, 0));
+	}
+	void Update()
+	{
+	}
+
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Player"))          // Sienen piilottaminen kun siihen osuu.
+		{
+			StartCoroutine(coRoutineTest());
+
+
+		}
+
+
+
+	}
+
+	IEnumerator coRoutineTest()
+	{
+		Debug.Log("Uusi sieni");
+		yield return new WaitForSeconds(3);
+		randomInt = Random.Range(0, spawnees.Length);
+		Instantiate(spawnees[randomInt], spawnPos.position, Quaternion.Euler(270, 0, 0));
+
+	}
+}

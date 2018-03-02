12fdae21f47980b24e4887f5d96845f7c24c9186
VeniBoi <35433655+VeniBoi@users.noreply.github.com>
2018-03-02 22:09:15 +0200

    Veni
    Toimiva MiinaKasa

--- UNITY/Pelikurssi/Assets/Scripts/Miina.cs
+++ UNITY/Pelikurssi/Assets/Scripts/Miina.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections;
+﻿//Spagettikoodi a'la Veni
+//Use at your own risk. :-D
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,7 +10,7 @@ public class Miina : MonoBehaviour {
 
 	public GameObject OsumaEfekti;
 	public GameObject Mesh;
-	public GameObject KameraStop;
+	
 
 	// Use this for initialization
 	void Start () {
@@ -24,18 +27,21 @@ public class Miina : MonoBehaviour {
 	{
 		if (other.gameObject.CompareTag("Player"))
 		{
+			
 			Instantiate(OsumaEfekti, transform.position, transform.rotation);
 			Debug.Log("PAM!");
 			Destroy(GameObject.Find("Pelaaja"));
 			MeshRenderer m = Mesh.GetComponent<MeshRenderer>();
 			m.enabled = false;
-			KameraStop.GetComponent<ThirdPersonCamera>().enabled = false;
+			GameObject.Find("Main Camera").GetComponent<ThirdPersonCamera>().enabled=false;
+			Debug.Log("Nyt pitäisi tulla Loppu");
+
 
 			StartCoroutine(LoppuLataus());
 
-			
 
-			
+
+
 		}
 	}
 
@@ -43,8 +49,9 @@ public class Miina : MonoBehaviour {
 
 	IEnumerator LoppuLataus()
 	{
-
+		Debug.Log("Toimiiko odotus?");
 		yield return new WaitForSeconds(3.5f);
+		Debug.Log("Loppu ladattu");
 		SceneManager.LoadScene("Loppu");
 		
 
---
+++ UNITY/Pelikurssi/Assets/Scripts/VaaraTuhoaminen.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VaaraTuhoaminen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(Tuhous());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+	IEnumerator Tuhous()
+	{
+		yield return new WaitForSeconds(60);
+		Destroy(gameObject);
+	}
+}
+
+

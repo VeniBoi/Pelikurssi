b04250b262e884417ec569356d6a65a634725cc7
VeniBoi <35433655+VeniBoi@users.noreply.github.com>
2018-03-08 00:19:40 +0200

    Veni
    Sieni PopUp

--- UNITY/Pelikurssi/Assets/Scripts/SieniNoukinta.cs
+++ UNITY/Pelikurssi/Assets/Scripts/SieniNoukinta.cs
@@ -18,6 +18,7 @@ public class SieniNoukinta : MonoBehaviour
 	static public int Keltavahvero;
 	static public int score;
 	public Text countText;
+	public Animator anim;
 
 	// Use this for initialization
 	void Start()
@@ -41,13 +42,16 @@ public class SieniNoukinta : MonoBehaviour
 	{
 		if (other.gameObject.CompareTag("Karpassieni"))          
 		{
+			GameObject.Find("SieniPickupKarpa").GetComponent<Text>().enabled = true;
+			GameObject.Find("SieniPickupKarpa").GetComponent<Animator>().enabled = true;
+
 			Debug.Log("Kärpässieni noukittu!");
             Destroy(other.gameObject);                      // Sienen tuhoaminen kun siihen osuu.
 			score = score + -50;								// Scoren lisääminen.
 			SetCountText();
 			GameObject.Find("Pelaaja").GetComponent<PlayerController>().runSpeed = 0.1f;
 			GameObject.Find("Pelaaja").GetComponent<PlayerController>().animator.Play("Noukkiminen");
-			StartCoroutine(SieniPysäytys());
+			StartCoroutine(SieniPysäytysKarpa());
 			Kärpässieni++;
 			
 
@@ -55,42 +59,57 @@ public class SieniNoukinta : MonoBehaviour
 
 		else if (other.gameObject.CompareTag("Keltavahvero"))
 		{
+			GameObject.Find("SieniPickupKelta").GetComponent<Text>().enabled = true;
+			GameObject.Find("SieniPickupKelta").GetComponent<Animator>().enabled = true;
+
 			Debug.Log("Kanttarelli noukittu!");
             Destroy(other.gameObject);
             score = score + 90;
 			SetCountText();
 			GameObject.Find("Pelaaja").GetComponent<PlayerController>().runSpeed = 0.1f;
 			GameObject.Find("Pelaaja").GetComponent<PlayerController>().animator.Play("Noukkiminen");
-			StartCoroutine(SieniPysäytys());
+			StartCoroutine(SieniPysäytysKelta());
 			Keltavahvero++;
 		}
 
 		else if (other.gameObject.CompareTag("Haaparousku"))
 		{
+			GameObject.Find("SieniPickupHaapa").GetComponent<Text>().enabled = true;
+			GameObject.Find("SieniPickupHaapa").GetComponent<Animator>().enabled = true;
+
 			Debug.Log("Haaparousku noukittu!");
             Destroy(other.gameObject);
             score = score + 70;
 			SetCountText();
 			GameObject.Find("Pelaaja").GetComponent<PlayerController>().runSpeed = 0.1f;
 			GameObject.Find("Pelaaja").GetComponent<PlayerController>().animator.Play("Noukkiminen");
-			StartCoroutine(SieniPysäytys());
+			StartCoroutine(SieniPysäytysHaapa());
 			Haaparousku++;
+			
+
 		}
 
 		else if (other.gameObject.CompareTag("Herkkutatti"))
 		{
+			GameObject.Find("SieniPickupHerkku").GetComponent<Text>().enabled = true;
+			GameObject.Find("SieniPickupHerkku").GetComponent<Animator>().enabled = true;
+
 			Debug.Log("Herkkutatti noukittu!");
             Destroy(other.gameObject);
             score = score + 40;
 			SetCountText();
 			GameObject.Find("Pelaaja").GetComponent<PlayerController>().runSpeed = 0.1f;
 			GameObject.Find("Pelaaja").GetComponent<PlayerController>().animator.Play("Noukkiminen");
-			StartCoroutine(SieniPysäytys());		
+			StartCoroutine(SieniPysäytysHerkku());
 			Herkkutatti++;
+			
 		}
 
 		else if (other.gameObject.CompareTag("PsiloSieni"))
 		{
+			GameObject.Find("SieniPickupPsilo").GetComponent<Text>().enabled = true;
+			GameObject.Find("SieniPickupPsilo").GetComponent<Animator>().enabled = true;
+
 			Debug.Log("Psilosieni noukittu!");
             Destroy(other.gameObject);
             score = score + -70;
@@ -98,6 +117,7 @@ public class SieniNoukinta : MonoBehaviour
 			GameObject.Find("Pelaaja").GetComponent<PlayerController>().runSpeed *= 2f;
 			GameObject.Find("Pelaaja").GetComponent<PlayerController>().animator.Play("Noukkiminen");
 			Psilosieni++;
+			StartCoroutine(SieniPysäytysPsilo());
 		}
 
 		
@@ -111,13 +131,65 @@ public class SieniNoukinta : MonoBehaviour
 
 	}
 
-	IEnumerator SieniPysäytys()		//Pysäyttää hahmon hetkeksi kun poimii sienen.
+	IEnumerator SieniPysäytysHaapa()		//Pysäyttää hahmon hetkeksi kun poimii sienen.
 	{
 		yield return new WaitForSeconds(0.2f);
-		GameObject.Find("Pelaaja").GetComponent<PlayerController>().runSpeed = 8f;		
+		GameObject.Find("Pelaaja").GetComponent<PlayerController>().runSpeed = 8f;
+		yield return new WaitForSeconds(1.6f);
+	
+		anim.Play("SieniAnimation", 0, 0);
+		GameObject.Find("SieniPickupHaapa").GetComponent<Text>().enabled = false;
+		GameObject.Find("SieniPickupHaapa").GetComponent<Animator>().enabled = false;
+		
 	}
 
+	IEnumerator SieniPysäytysKarpa()        //Pysäyttää hahmon hetkeksi kun poimii sienen.
+	{
+		yield return new WaitForSeconds(0.2f);
+		GameObject.Find("Pelaaja").GetComponent<PlayerController>().runSpeed = 8f;
+		yield return new WaitForSeconds(1.6f);
 
-	
+		anim.Play("SieniAnimation", 0, 0);
+		GameObject.Find("SieniPickupKarpa").GetComponent<Text>().enabled = false;
+		GameObject.Find("SieniPickupKarpa").GetComponent<Animator>().enabled = false;
+
+	}
+
+	IEnumerator SieniPysäytysPsilo()        //Pysäyttää hahmon hetkeksi kun poimii sienen.
+	{
+		yield return new WaitForSeconds(0.2f);
+		GameObject.Find("Pelaaja").GetComponent<PlayerController>().runSpeed = 8f;
+		yield return new WaitForSeconds(1.6f);
+
+		anim.Play("SieniAnimation", 0, 0);
+		GameObject.Find("SieniPickupPsilo").GetComponent<Text>().enabled = false;
+		GameObject.Find("SieniPickupPsilo").GetComponent<Animator>().enabled = false;
+
+	}
+
+	IEnumerator SieniPysäytysHerkku()        //Pysäyttää hahmon hetkeksi kun poimii sienen.
+	{
+		yield return new WaitForSeconds(0.2f);
+		GameObject.Find("Pelaaja").GetComponent<PlayerController>().runSpeed = 8f;
+		yield return new WaitForSeconds(1.6f);
+
+		anim.Play("SieniAnimation", 0, 0);
+		GameObject.Find("SieniPickupHerkku").GetComponent<Text>().enabled = false;
+		GameObject.Find("SieniPickupHerkku").GetComponent<Animator>().enabled = false;
+
+	}
+
+	IEnumerator SieniPysäytysKelta()        //Pysäyttää hahmon hetkeksi kun poimii sienen.
+	{
+		yield return new WaitForSeconds(0.2f);
+		GameObject.Find("Pelaaja").GetComponent<PlayerController>().runSpeed = 8f;
+		yield return new WaitForSeconds(1.6f);
+
+		anim.Play("SieniAnimation", 0, 0);
+		GameObject.Find("SieniPickupKelta").GetComponent<Text>().enabled = false;
+		GameObject.Find("SieniPickupKelta").GetComponent<Animator>().enabled = false;
+
+	}
+
 }
 	

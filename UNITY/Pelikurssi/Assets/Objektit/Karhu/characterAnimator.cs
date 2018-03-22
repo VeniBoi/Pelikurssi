e8bba05afe0416b4e7f0b8bbad6813bfd8e592eb
VeniBoi <35433655+VeniBoi@users.noreply.github.com>
2018-03-22 21:54:02 +0200

    Veni
    Karhu ja animaatiot

---
+++ UNITY/Pelikurssi/Assets/Objektit/Karhu/characterAnimator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class characterAnimator : MonoBehaviour {
+
+
+	const float locomotionAnimationSmoothTime = 0.1f;
+	Animator animator;
+	NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+
+		agent = GetComponent<NavMeshAgent>();
+		animator = GetComponent<Animator>();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float speedPercent = agent.velocity.magnitude / agent.speed;
+		animator.SetFloat("speedPercent", speedPercent, locomotionAnimationSmoothTime, Time.deltaTime);
+	}
+}
